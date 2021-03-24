e872f22f5b03601763454615c1adef5c564ee844
Qujohujo <quinnjpete@gmail.com>
2021-03-24 11:39:05 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieDbContext context { get; set; }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, MovieDbContext con)
+        {
+            _logger = logger;
+            context = con;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EnterFilm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EnterFilm(Movie movie)
+        {
+            if (movie.Title == "Independence Day")
+            {
+                ModelState.AddModelError(string.Empty, "That movie is not valid. Please try any other movie.");
+                return View();
+            }
+            else if (ModelState.IsValid)
+            {
+                //add movie to database
+                context.Movies.Add(movie);
+                context.SaveChanges();
+                return View("FilmList", context.Movies);
+            }
+            else
+            {
+                //This will include basic error messages given from the model
+                return View();
+            }
+
+        }
+
+        public IActionResult FilmList()
+        {
+            return View(context.Movies);
+        }
+
+
+        [HttpGet]
+        public IActionResult EditFilm(int movieId)
+        {
+            Movie movie = context.Movies.FirstOrDefault(m => m.MovieId == movieId);
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult EditFilm(Movie movie)
+        {
+            if (movie.Title == "Independence Day")
+            {
+                ModelState.AddModelError(string.Empty, "That movie is not valid. Please try any other movie.");
+                return View();
+            }
+            else if (ModelState.IsValid)
+            {
+                //update movie in database
+                context.Movies.Update(movie);
+                context.SaveChanges();
+                return View("FilmList", context.Movies);
+            }
+            else
+            {
+                //This will include basic error messages given from the model
+                return View();
+            }
+        }
+
+
+
+        //receives the movie id from the form, deletes that entry, and saves the changes to the database
+        [HttpPost]
+        public IActionResult DeleteFilm(int movieId)
+        {
+            context.Movies.Remove(context.Movies.FirstOrDefault(m => m.MovieId == movieId));
+            context.SaveChanges();
+
+            return View("FilmList", context.Movies);
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollection.Models
+{
+    public class Movie
+    {
+        
+        //[ScaffoldColumn(false)]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The Year field is required")]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        public string? Edited { get; set; }
+
+        public string? LentTo { get; set; }
+
+        [StringLength(25, ErrorMessage = "Notes must be less than 25 characters.")]
+        public string? Notes { get; set; }
+    }
+}
